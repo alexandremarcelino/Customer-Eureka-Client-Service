c53166f9a428de51a97c9f96b01c4680ca2ff008
Alexandre Marcelino <alexandre.hoffmann.marcelino@gmail.com>
2023-11-25 09:41:36 -0300

    Customer-Eureka-Client-Service

---
+++ src/main/java/com/alexandre/marcelino/customer/CustomerApplication.java
@@ -0,0 +1,20 @@
+package com.alexandre.marcelino.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alexandre/marcelino/customer/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.alexandre.marcelino.customer.repository;
+
+import com.alexandre.marcelino.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
